eeb750d7cd1d70c4b4bbdbffad3333722cb183f3
Tupac Shakur <rasmus@wiegman.dk>
2024-01-18 18:17:50 +0100

    NS lookup cleaned

--- ns_lookup.py
+++ ns_lookup.py
@@ -11,18 +11,9 @@ with open(list) as my_file:
 for domain in domains:
     print(' ' )
     try:
-        #answersa = dns.resolver.resolve(domain.strip(), 'a')
-        answersns = dns.resolver.resolve(domain.strip(), 'NS')
-        #answersmx = dns.resolver.resolve(domain.strip(), 'MX')
-        #answerstxt = dns.resolver.resolve(domain.strip(), 'txt')
+       answersns = dns.resolver.resolve(domain.strip(), 'NS')
     except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers):
             print(domain.strip(), 'Does not have any NS Record')
     else:
-        #for ipval in answersa:
-        #    print(domain.strip(), '- A -', ipval.to_text())
         for ipval in answersns:
             print(domain.strip(), '- NS -', ipval.to_text())
-       # for ipval in answersmx:
-       #     print(domain.strip(), '- MX -', ipval.to_text())
-       # for ipval in answerstxt:
-       #     print(domain, '- TXT - ', ipval.to_text())
